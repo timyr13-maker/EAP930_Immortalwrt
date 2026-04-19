a28ec19f03c683976112abd5354e60984596487b
Timur <timyr13@gmail.com>
2026-04-19 21:41:23 +0300

    Finalize EAP930 port: add stock compatibility, factory image support, and cleanup

--- openwrt_port/target/linux/mediatek/image/netis_eap930.mk
+++ openwrt_port/target/linux/mediatek/image/netis_eap930.mk
@@ -4,6 +4,9 @@ define Device/netis_eap930
   DEVICE_DTS := mt7981b-netis-eap930
   DEVICE_DTS_LOADADDR := 0x47000000
 
+  # Stock compatibility for migration via sysupgrade
+  SUPPORTED_DEVICES += mt7981-spim-nand-rfb-pcb7605-1G
+
   DEVICE_PACKAGES := kmod-mt7915e kmod-mt7981-firmware mt7981-wo-firmware \
                      kmod-leds-gpio kmod-gpio-button-hotplug
 
@@ -16,8 +19,9 @@ define Device/netis_eap930
   # Reserve ~2 MB for UBIFS overhead
   IMAGE_SIZE := 115200k
 
-  IMAGES += sysupgrade.bin
+  IMAGES += sysupgrade.bin factory.bin
   IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+  IMAGE/factory.bin := append-kernel | pad-to $$$$(BLOCKSIZE) | append-ubi
 
   DEVICE_COMPAT_VERSION := 1.0
 endef
